999b4cb7ce7af635eaa5c3288812a767805a6275
Thomas-Durand-Texte <thomas.durandtexte@protonmail.com>
2023-05-19 07:46:25 +0200

    gunicornUpdate

--- install_app.sh
+++ install_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# sudo apt update
-# sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv
+sudo apt update
+sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv
 
 python3 -m venv .APIenv
 source .APIenv/bin/activate
