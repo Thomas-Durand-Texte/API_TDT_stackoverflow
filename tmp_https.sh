2986fedbed8bc8d7338ce6c2dc7ed5b7bbf54d96
Thomas <thomas@2a01cb058bf0370013caefe4bf382084.ipv6.abo.wanadoo.fr>
2023-05-28 09:17:56 +0200

    upload

--- tmp_https.sh
+++ tmp_https.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 projectname=API_TDT_stackoverflow
+
 sudo cp ./setup_https/${projectname}.service /etc/systemd/system 
 sudo systemctl daemon-reload
 sudo systemctl start ${projectname}
@@ -7,6 +8,8 @@ sudo systemctl enable ${projectname}
 
 sudo systemctl status ${projectname}
 
-# sudo cp ./setup_https/API_TDT_stackoverflow /etc/nginx/sites-available
-# sudo ln -s /etc/nginx/sites-available/API_TDT_stackoverflow /etc/nginx/sites-enabled
+sudo cp ./setup_https/${projectname} /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available/${projectname} /etc/nginx/sites-enabled
+
+sudo nginx -t
 
