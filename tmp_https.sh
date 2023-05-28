eaa6136daba92f4ac9a4ed61de15a0780acc4800
Thomas <thomas@2a01cb058bf0370013caefe4bf382084.ipv6.abo.wanadoo.fr>
2023-05-28 09:03:51 +0200

    upload

--- tmp_https.sh
+++ tmp_https.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-sudo cp ./setup_https/API_stackoverflow.service /etc/systemd/system
-
+sudo cp ./setup_https/API_TDT_stackoverflow.service /etc/systemd/system 
 sudo systemctl start API_TDT_stackoverflow
 sudo systemctl enable API_TDT_stackoverflow
 
